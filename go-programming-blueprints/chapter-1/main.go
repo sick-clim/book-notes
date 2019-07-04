1a6fcdffdec6b3097e92149e5525f247196439ed
sick-clim <46617682+sick-clim@users.noreply.github.com>
2019-07-05 00:20:25 +0900

    chapter1.1 template

--- go-programming-blueprints/chapter-1/main.go
+++ go-programming-blueprints/chapter-1/main.go
@@ -3,22 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>chat</title>
-				</head>
-				<body>
-					chat.
-				</body>
-			</html>
-		`))
-	})
+// templeは1つのテンプレートを表す
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
 
+// ServerHTTP はHTTPリクエストを処理する
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// Start web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServer:", err)
