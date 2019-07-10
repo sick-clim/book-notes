0d248b8fa120835e477ae0fcbd6ee72157f2e22c
sick-clim <46617682+sick-clim@users.noreply.github.com>
2019-07-10 23:54:19 +0900

    Chapter1.2.2 add room struct

--- go-programming-blueprints/chapter-1/room.go
+++ go-programming-blueprints/chapter-1/room.go
@@ -3,4 +3,10 @@ package main
 type room struct {
 	// 他のクライアントに転送するためのメッセージを保持するチャネル
 	forward chan []byte
+	// チャットルームに参加しようとしているクライアントのためのチャネル
+	join chan *client
+	// チャットルームから退出しようとしているクライアントのためのチャネル
+	leave chan *client
+	// 在室しているすべてのクライアントが保持されます
+	clients map[*client]bool
 }
