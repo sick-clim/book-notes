ce3b9853c1a23a30e683ce677755fb6d755e3d62
sick-clim <46617682+sick-clim@users.noreply.github.com>
2019-07-11 00:06:28 +0900

    Add run method

--- go-programming-blueprints/chapter-1/room.go
+++ go-programming-blueprints/chapter-1/room.go
@@ -10,3 +10,29 @@ type room struct {
 	// 在室しているすべてのクライアントが保持されます
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leav:
+			// 退出
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// 全てのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージ送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
